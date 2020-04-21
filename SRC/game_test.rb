48386d9f6f8e95736eacaac37346c2285a6d24f4
Tim <tims111090@live.com>
2020-04-21 18:34:31 +1000

    added welcome screen with logo

---
+++ SRC/game_test.rb
@@ -0,0 +1,10 @@
+require "test/unit"
+
+require_relative "welcome.rb"
+
+
+class GameTest < Test::Unit::TestCase
+    def 
+    end
+end
+
--- SRC/welcome.rb
+++ SRC/welcome.rb
@@ -2,19 +2,47 @@
 require "figlet"
 require "tty-prompt"
 
+
 class Welcome_screen
-    def welcome
+    def welcome_one
         # a = Artii::Base.new
         system("clear")
-        # puts a.asciify("HANGMAN") 
-        system "figlet -c HANGMAN "
-        prompt1 = TTY::Prompt.new
-        prompt1.keypress("----------------------- PRESS ANY KEY TO HANGMAN --------------------------\n")
+        # puts a.asciify("HANGMAN")
+        system "figlet -c HANGMAN"
+        puts "          ```````````````````````````````````````````````````````````"     
+        puts "                                 __________
+                                 |/      |
+                                 |      (_)
+                                 |      \\|/
+                                 |       |
+                                 |      / \\
+                                 |
+                                _|___"
+        prompt_one = TTY::Prompt.new
+        prompt_one.keypress("\n----------------------- PRESS ANY KEY TO HANG THE MAN --------------------------\n")
     end
 
+    def welcome_two
+        # a = Artii::Base.new
+        system("clear")
+        # puts a.asciify("HANGMAN")
+        system "figlet -c HANGMAN"
+        puts "          ```````````````````````````````````````````````````````````"     
+        puts "                                 __________
+                                 |/      |
+                                 |       |
+                                 |      (_)
+                                 |      \\|/
+                                 |       |
+                                 |      / \\
+                                _|___"
+        prompt_one = TTY::Prompt.new
+        prompt_one.keypress("\n----------------------- PRESS ANY KEY TO CONTINUE --------------------------\n")
+    end
 end
 
 
 
 welcome_screen = Welcome_screen.new
-@welcome = welcome_screen.welcome
+@welcome = welcome_screen.welcome_one
+@welcome = welcome_screen.welcome_two
